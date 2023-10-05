502a6bcee9fd916e857eca9f9771d6af764f913e
fedykvitalik2004 <90273574+fedykvitalik2004@users.noreply.github.com>
2023-10-05 10:31:53 +0300

    added second and third reports

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -6,7 +6,7 @@ I
 lab_works/1/calculator.py,8/5/858e2f52ab2d4af3e0eb3d188c4db51a3890d6de
 I
 lab_works/2/calculator.py,9/c/9c95ee9eb66d54aa9ae111752def4d97166bfbde
-B
-lab_works/3/man.py,9/9/9931520679c26f826d04598ece94cd1ec0e8106f
 F
 lab_works/3/output.txt,6/b/6bae9b1811ac7db90c118cf5786316d0b5213fb2
+R
+"lab_works/3/ascii_art_generator.py,f/f/ffef09b8db83e70da921387190935497dcdf1b81
--- lab_works/2/calculator.py
+++ lab_works/2/calculator.py
@@ -3,8 +3,8 @@ import math
 class Calculator:
 
     def __init__(self):
-        self.first_value = None
-        self.operator = None
+        self.__first_value = None
+        self.__operator = None
         self.second_value = None
 
     def run(self):
@@ -23,10 +23,10 @@ class Calculator:
     def input(self):
         try:
             self.__init__()
-            self.first_value = float(input("Enter the first value: "))
-            self.operator = str(input("Enter the operator ['+', '-', '*', '/', '**', '√', '%']: "))
-            if self.__is_operator_correct(self.operator):
-                if self.operator != "√":
+            self.__first_value = float(input("Enter the first value: "))
+            self.__operator = str(input("Enter the operator ['+', '-', '*', '/', '**', '√', '%']: "))
+            if self.__is_operator_correct(self.__operator):
+                if self.__operator != "√":
                     self.second_value = float(input("Enter the second value: "))
             else:
                 raise RuntimeWarning("The input operator is incorrect")
@@ -39,24 +39,24 @@ class Calculator:
         return operator in valid_operators
 
     def calculate(self):
-        if self.operator == "+":
-            return self.first_value + self.second_value
-        elif self.operator == "-":
-            return self.first_value - self.second_value
-        elif self.operator == "*":
-            return self.first_value * self.second_value
-        elif self.operator == "/":
+        if self.__operator == "+":
+            return self.__first_value + self.second_value
+        elif self.__operator == "-":
+            return self.__first_value - self.second_value
+        elif self.__operator == "*":
+            return self.__first_value * self.second_value
+        elif self.__operator == "/":
             if self.second_value == 0:
                 raise ZeroDivisionError("Impossible to divide")
-            return self.first_value / self.second_value
-        elif self.operator == "**":
-            return self.first_value ** self.second_value
-        elif self.operator == "√":
-            if self.first_value < 0:
+            return self.__first_value / self.second_value
+        elif self.__operator == "**":
+            return self.__first_value ** self.second_value
+        elif self.__operator == "√":
+            if self.__first_value < 0:
                 raise ArithmeticError("Number is negative, therefore it is impossible to  calculate the square root")
-            return math.sqrt(self.first_value)
-        elif self.operator == "%":
-            return self.first_value % self.second_value
+            return math.sqrt(self.__first_value)
+        elif self.__operator == "%":
+            return self.__first_value % self.second_value
 
 
 c = Calculator()
